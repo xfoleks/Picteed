daf65f702366193907ccb8786bc8390714c0f4c4
Oleksandr Filonenko <filonenko.oleksa@gmail.com>
2022-07-07 10:59:27 +0300

    Added DI and Cache parts to project

---
+++ app/src/main/java/edu/xfolex/picteed/di/DatabaseModule.kt
@@ -0,0 +1,30 @@
+package edu.xfolex.picteed.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import edu.xfolex.picteed.data.local.database.UnsplashDatabase
+import edu.xfolex.picteed.util.Constants.UNSPLASH_DATABASE
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ): UnsplashDatabase {
+        return Room.databaseBuilder(
+            context,
+            UnsplashDatabase::class.java,
+            UNSPLASH_DATABASE
+        ).build()
+    }
+
+}
